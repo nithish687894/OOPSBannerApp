1356d152f08dcd7338355f00d1c06f6ce256f3cc
Nithishkumar S <nithish687894@gmail.com>
2026-02-21 19:00:26 +0530

    UC5: Inline Array Initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,19 @@
 /**
- * OOPSBannerApp - UC4: Array & Loop
+ * OOPSBannerApp - UC5: Inline Array Init
  * @author NithishKumar
- * @version 4.0
+ * @version 5.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] bannerLines = new String[7];
-
-        bannerLines[0] = String.join(" ", " *****  ", " *****  ", "****   ", " *****  ");
-        bannerLines[1] = String.join(" ", "*     * ", "*     * ", "*   *  ", "*       ");
-        bannerLines[2] = String.join(" ", "*     * ", "*     * ", "*   *  ", "*       ");
-        bannerLines[3] = String.join(" ", "*     * ", "*     * ", "****   ", " *****  ");
-        bannerLines[4] = String.join(" ", "*     * ", "*     * ", "*      ", "      * ");
-        bannerLines[5] = String.join(" ", "*     * ", "*     * ", "*      ", "      * ");
-        bannerLines[6] = String.join(" ", " *****  ", " *****  ", "*      ", " *****  ");
+        String[] bannerLines = {
+            String.join(" ", " *****  ", " *****  ", "****   ", " *****  "),
+            String.join(" ", "*     * ", "*     * ", "*   *  ", "*       "),
+            String.join(" ", "*     * ", "*     * ", "*   *  ", "*       "),
+            String.join(" ", "*     * ", "*     * ", "****   ", " *****  "),
+            String.join(" ", "*     * ", "*     * ", "*      ", "      * "),
+            String.join(" ", "*     * ", "*     * ", "*      ", "      * "),
+            String.join(" ", " *****  ", " *****  ", "*      ", " *****  ")
+        };
 
         for (String line : bannerLines) {
             System.out.println(line);
