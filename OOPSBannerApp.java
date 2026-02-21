4992e58b2548ec0233da67c1f3d5b9f855b13fd3
Nithishkumar S <nithish687894@gmail.com>
2026-02-21 18:57:30 +0530

    UC2: Print Banner using String concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,16 @@
 /**
- * OOPSBannerApp - UC1: Print OOPS to Console
+ * OOPSBannerApp - UC2: Print Banner
  * @author NithishKumar
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" *****  " + " *****  " + "****   " + " *****  ");
+        System.out.println("*     * " + "*     * " + "*   *  " + "*       ");
+        System.out.println("*     * " + "*     * " + "*   *  " + "*       ");
+        System.out.println("*     * " + "*     * " + "****   " + " *****  ");
+        System.out.println("*     * " + "*     * " + "*      " + "      * ");
+        System.out.println("*     * " + "*     * " + "*      " + "      * ");
+        System.out.println(" *****  " + " *****  " + "*      " + " *****  ");
     }
 }
